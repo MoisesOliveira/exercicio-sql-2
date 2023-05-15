d85c08a1dffa96966d2800aad95855a14575a5c2
Moises Oliveira <48396278+MoisesOliveira@users.noreply.github.com>
2023-05-15 18:42:41 -0300

    Add files via upload

---
+++ exercicio5-1.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE exjoin1
+USE exjoin1
+GO
+CREATE TABLE projects(
+	id INT PRIMARY KEY IDENTITY(1001,1),
+	nome VARCHAR(45),
+	descr VARCHAR(45),
+	data_proj DATE CHECK(data_proj > '2014-09-01'))
+GO
+CREATE TABLE users(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(45),
+	username VARCHAR(45) CONSTRAINT uniq_username UNIQUE,
+	senha VARCHAR(45) DEFAULT('123mudar'),
+	email VARCHAR(45)
+)
+GO
+CREATE TABLE users_has_projects(
+	users_id INT,
+	projects_id INT,
+	PRIMARY KEY(users_id, projects_id),
+	FOREIGN KEY(users_id) REFERENCES users(id),
+	FOREIGN KEY(projects_id) REFERENCES projects(id)
+)
+
+ALTER TABLE users DROP CONSTRAINT uniq_username
+
+ALTER TABLE users ADD CONSTRAINT uniq_username UNIQUE(username)
+
+ALTER TABLE users ALTER COLUMN senha VARCHAR(8)
+
+SELECT * FROM users
+SELECT * FROM projects
+SELECT * FROM users_has_projects
+
+UPDATE projects SET data_proj = '2014-09-12' WHERE nome LIKE 'Manutenção%'
+
+UPDATE users SET username = 'Rh_cido' WHERE nome LIKE 'Aparecido%'
+
+UPDATE users SET senha = CASE 
+WHEN senha = '123mudar' THEN '888@*' 
+ELSE senha END
+WHERE username = 'Rh_maria'
+
+DELETE FROM users_has_projects WHERE users_id = 2
+
+INSERT INTO users VALUES ('Joao', 'Ti_joao', '123mudar', 'joao@empresa.com')
+
+INSERT INTO projects VALUES ('Atualização de Sistemas', 'Modificação de Sistemas Operacionais nos PC´s','2014-09-12')
+
+SELECT users.id, users.nome, users.email,projects.nome, projects.descr,projects.data_proj FROM users, 
+projects, users_has_projects 
+WHERE users.id = users_has_projects.users_id AND projects.id = users_has_projects.projects_id AND projects.nome = 'Re-folha'
+
+SELECT projects.nome, users_has_projects.users_id FROM projects LEFT OUTER JOIN 
+users_has_projects ON projects.id = users_has_projects.projects_id WHERE users_has_projects.users_id IS NULL
+
+SELECT users.nome, users_has_projects.projects_id FROM users LEFT OUTER JOIN 
+users_has_projects ON users.id = users_has_projects.users_id WHERE users_has_projects.projects_id IS NULL
